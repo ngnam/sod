28385f0baeb8a20c68bcc1f8ce5345fb6c62a7b8
ngnam <nguyenvannam0411@gmail.com>
2020-05-08 22:41:41 +0700

    add support form-body request for api POST

--- source/StoreOrder/src/StoreOrder.WebApplication/Controllers/AccountController.cs
+++ source/StoreOrder/src/StoreOrder.WebApplication/Controllers/AccountController.cs
@@ -11,6 +11,7 @@ using System.Threading.Tasks;
 namespace StoreOrder.WebApplication.Controllers
 {
     [ApiVersion("1")]
+    [ApiVersion("2")]
     public class AccountController : ApiBaseController
     {
         private readonly IAuthRepository _authRepository;
@@ -54,6 +55,34 @@ namespace StoreOrder.WebApplication.Controllers
             }
         }
 
+        [HttpPost("login/staff"), MapToApiVersion("2")]
+        [AllowAnonymous]
+        public async Task<IActionResult> LoginV2([FromForm] UserLoginAuthenticate model)
+        {
+            if (HttpContext.User.Identity.IsAuthenticated)
+            {
+                return Ok(new { isAuthenicated = HttpContext.User.Identity.IsAuthenticated });
+            }
+
+            var user = await _authRepository.GetUserByUserNameOrEmail(model.UserNameOrEmail);
+            if (user == null)
+            {
+                throw new ApiException("Wrong username or email.", (int)HttpStatusCode.BadRequest);
+            }
+            else if (!_authRepository.VerifyPasswordHash(model.Password, user.HashPassword, user.SaltPassword))
+            {
+                throw new ApiException("Wrong password.", (int)HttpStatusCode.BadRequest);
+            }
+            else
+            {
+                string currentUserId = Guid.NewGuid().ToString();
+                var userLogined = _authRepository.GenerateToken(user, currentUserId);
+                // save to login
+                await _authRepository.SaveToUserLoginAsync(user, userLogined, currentUserId);
+                return Ok(userLogined);
+            }
+        }
+
         [HttpGet("logout"), MapToApiVersion("1")]
         [Authorize]
         public async Task<IActionResult> Logout()
@@ -65,6 +94,16 @@ namespace StoreOrder.WebApplication.Controllers
         [HttpPost("login/customer"), MapToApiVersion("1")]
         [AllowAnonymous]
         public async Task<IActionResult> LoginAndRegisterCustomer([FromBody] CustomerLoginDTO model)
+        {
+            var userLogined = new UserLogined();
+            if (ModelState.IsValid)
+                userLogined = await _authRepository.SignInAndSignUpCustomerAsync(model);
+            return Ok(userLogined);
+        }
+
+        [HttpPost("login/customer"), MapToApiVersion("2")]
+        [AllowAnonymous]
+        public async Task<IActionResult> LoginAndRegisterCustomerV2([FromForm] CustomerLoginDTO model)
         {
             var userLogined = new UserLogined();
             if (ModelState.IsValid)
