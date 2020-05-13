bc0c472973e6f7aab7418b8e2f476a2cddd44e8f
ngnam <nguyenvannam0411@gmail.com>
2020-05-13 16:36:08 +0700

    support formdata for api UpdateSettingsV2

--- source/StoreOrder/src/StoreOrder.WebApplication/Controllers/ConfigController.cs
+++ source/StoreOrder/src/StoreOrder.WebApplication/Controllers/ConfigController.cs
@@ -19,6 +19,7 @@ namespace StoreOrder.WebApplication.Controllers
     [Route("[controller]")]
     [ApiController]
     [ApiVersion("1")]
+    [ApiVersion("2")]
     public class ConfigController : ControllerBase
     {
         private readonly ILogger<ConfigController> _logger;
@@ -83,6 +84,64 @@ namespace StoreOrder.WebApplication.Controllers
                     settingExist.SettingValueVarchar = model.SettingValue;
                     settingExist.SettingDesc = model.SettingDesc;
                 } else {
+                    // other Create
+                    Setting setting = new Setting();
+                    setting.CreatedBy = AuthenticationId;
+                    setting.LastUpdatedBy = AuthenticationId;
+                    setting.SettingType = typeSetting;
+                    setting.SettingKey = model.SettingKey;
+                    setting.SettingValueVarchar = model.SettingValue;
+                    _context.Settings.Add(setting);
+                }
+
+                // save to database
+                try
+                {
+                    await _context.SaveChangesAsync();
+                }
+                catch (System.Exception ex)
+                {
+                    _logger.Log(LogLevel.Warning, "Có lỗi xảy ra khi update Settings", ex.Message);
+#if DEBUG
+                    throw new ApiException(ex);
+#else
+                    throw new ApiException("Có lỗi xảy ra khi update database");
+#endif
+                }
+
+            }
+
+            return Ok(1);
+        }
+
+        [HttpPost("environment/{settingType}"), MapToApiVersion("2")]
+        public async Task<IActionResult> UpdateSettingsV2([FromForm] SettingDTO model, [FromHeader] string AuthenticationId, [FromHeader] string UserAgent, string settingType = "employee")
+        {
+            if (string.IsNullOrEmpty(AuthenticationId) || string.IsNullOrEmpty(UserAgent))
+            {
+                throw new ApiException("Bạn không có quyền truy cập.", (int)HttpStatusCode.Unauthorized);
+            }
+            if (ModelState.IsValid)
+            {
+                model.SettingKey = model.SettingKey.Trim();
+                int typeSetting = settingType == "employee" ? (int)TypeSetting.EMPLPLOYEE : (int)TypeSetting.CUSTOMER;
+                var settingExist = _context.Settings.FirstOrDefault(s =>
+                        s.SettingKey.ToLower().Equals(model.SettingKey.Trim().ToLower()) &&
+                        s.SettingType == typeSetting &&
+                        s.CreatedBy.Equals(AuthenticationId));
+
+                // if settingKey exist 
+                if (settingExist != null)
+                {
+                    // update
+                    _context.Entry(settingExist).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                    settingExist.LastUpdated = DateTime.UtcNow;
+                    settingExist.LastUpdatedBy = AuthenticationId;
+                    settingExist.SettingValueVarchar = model.SettingValue;
+                    settingExist.SettingDesc = model.SettingDesc;
+                }
+                else
+                {
                     // other Create
                     Setting setting = new Setting();
                     setting.CreatedBy = AuthenticationId;
